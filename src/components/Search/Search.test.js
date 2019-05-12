56bbf8e3b39c9cf52c21b18cd7349df4ece4513f
ayushi <ayushi_nigam@mckinsey.com>
2019-05-12 16:31:54 +0530

    * added enzyme * updated test cases for search component

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 import Search from './index';
 
-it('renders correctly', () => {
-  const tree = renderer.create(<Search/>).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Search', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Search/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  const updateSearchValue = jest.fn();
+  const searchComponent = shallow(<Search updateSearchValue={updateSearchValue}/>)
+  it('should have an initial value as empty string', () => {
+    expect(searchComponent.instance().state.value).toEqual('');
+  });
+  it('should update value on Change', () => {
+    searchComponent.instance().onChange({target: {value: 'cat'}});
+    expect(searchComponent.instance().state.value).toEqual('cat');
+  });
+  it('updateValue: should trigger update search value', () => {
+    searchComponent.instance().setState({value: ''});
+    searchComponent.instance().updateValue();
+    expect(searchComponent.instance().props.updateSearchValue).toHaveBeenCalled();
+  });
+
 });
+
+
