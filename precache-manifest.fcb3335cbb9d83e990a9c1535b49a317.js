adac57454a92eeea399d51e7c9d4ccef7818b49b
ayushi <ayushi_nigam@mckinsey.com>
2019-05-14 01:50:21 +0530

    Updates

---
+++ precache-manifest.fcb3335cbb9d83e990a9c1535b49a317.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b028a467329f6fff2de3d3af9cf5236",
+    "url": "/Go-gif/index.html"
+  },
+  {
+    "revision": "754f966598f8ebeccfbe",
+    "url": "/Go-gif/static/css/main.8ae6f588.chunk.css"
+  },
+  {
+    "revision": "92a23989c75c2badc3f5",
+    "url": "/Go-gif/static/js/2.52333434.chunk.js"
+  },
+  {
+    "revision": "754f966598f8ebeccfbe",
+    "url": "/Go-gif/static/js/main.486fa0d0.chunk.js"
+  },
+  {
+    "revision": "2bfbbb4b9c7434035c3c",
+    "url": "/Go-gif/static/js/runtime~main.52789bff.js"
+  },
+  {
+    "revision": "15c492c623da3395346cf07996ff297e",
+    "url": "/Go-gif/static/media/appLogo.15c492c6.png"
+  },
+  {
+    "revision": "ca47aee8ddb9529e280f93c8e016cea8",
+    "url": "/Go-gif/static/media/pause.ca47aee8.svg"
+  },
+  {
+    "revision": "fd3f20172e808b23ca879b9511fd5118",
+    "url": "/Go-gif/static/media/play.fd3f2017.svg"
+  }
+]);
